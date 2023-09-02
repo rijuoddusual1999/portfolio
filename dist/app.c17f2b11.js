aab07e0789ebe7aeb8231893ad469c927f0f0307
Riju Mondal <pilotrijurocks@gmail.com>
2023-09-02 17:25:32 +0530

    unstable head with progress

--- dist/app.c17f2b11.js
+++ dist/app.c17f2b11.js
@@ -5625,7 +5625,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53832" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54987" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
