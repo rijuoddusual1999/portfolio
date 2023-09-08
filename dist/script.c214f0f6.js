d752a6480cffbe108afc489e42164c4bbc9b50fe
Riju Mondal <pilotrijurocks@gmail.com>
2023-09-08 18:32:37 +0530

    new update

---
+++ dist/script.c214f0f6.js
@@ -0,0 +1,12 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"hhCY":[function(require,module,exports) {
+
+},{}],"KHdE":[function(require,module,exports) {
+
+},{"./base.scss":"hhCY"}],"VEBd":[function(require,module,exports) {
+
+},{"./..\\images\\home_img.jpg":[["home_img.ebf00305.jpg","ycZS"],"ycZS"]}],"QZMV":[function(require,module,exports) {
+
+},{"./..\\images\\project_img.jpg":[["project_img.e271a9b8.jpg","fQZV"],"fQZV"],"./..\\images\\ross.webp":[["ross.deeff8d0.webp","qDME"],"qDME"],"./..\\images\\gitmo.jpeg":[["gitmo.b50d6c86.jpeg","IGE5"],"IGE5"],"./..\\images\\lou.jpg":[["lou.fb571df4.jpg","bb37"],"bb37"]}],"ikcD":[function(require,module,exports) {
+"use strict";require("../styles/base.scss"),require("../styles/loading.scss"),require("../styles/landing.scss"),require("../styles/parallax.scss"),require("../styles/project.scss"),require("../styles/home.scss");
+},{"../styles/base.scss":"hhCY","../styles/loading.scss":"KHdE","../styles/landing.scss":"VEBd","../styles/parallax.scss":"hhCY","../styles/project.scss":"QZMV","../styles/home.scss":"hhCY"}]},{},["ikcD"], null)
+//# sourceMappingURL=/script.c214f0f6.js.map
---
+++ src/Assets/js/OSC.js
@@ -0,0 +1,12 @@
+
+import hoverEffect from './hover-effect';
+
+
+var myAnimation = new hoverEffect({
+    parent: document.querySelector('#OSC'),
+    intensity: 0.3,
+    image1: '../images/osc1',
+    image2: '../images/osc2',
+    displacementImage: '../images/osc2'
+    
+});
