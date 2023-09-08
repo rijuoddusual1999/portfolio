b2793fec45045219a323a7dd3c99ca082a24feb2
Riju Mondal <pilotrijurocks@gmail.com>
2023-09-08 18:28:10 +0530

    Updates

---
+++ script.d0b3d319.js
@@ -0,0 +1,12 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"hhCY":[function(require,module,exports) {
+
+},{}],"KHdE":[function(require,module,exports) {
+
+},{"./base.scss":"hhCY"}],"VEBd":[function(require,module,exports) {
+
+},{"./..\\images\\pexels-jonathan-formento-4337198.jpg":[["pexels-jonathan-formento-4337198.48e9bc50.jpg","HMp7"],"HMp7"]}],"QZMV":[function(require,module,exports) {
+
+},{"./..\\images\\chester-wade-aVq_417KJ7U-unsplash.jpg":[["chester-wade-aVq_417KJ7U-unsplash.d7e17bd2.jpg","eCN8"],"eCN8"],"./..\\images\\1.jpg":[["1.ef5098b1.jpg","bO9D"],"bO9D"],"./..\\images\\2.jpg":[["2.4cde744e.jpg","lVDD"],"lVDD"],"./..\\images\\lou.jpg":[["lou.fb571df4.jpg","bb37"],"bb37"]}],"ikcD":[function(require,module,exports) {
+"use strict";require("../styles/base.scss"),require("../styles/loading.scss"),require("../styles/landing.scss"),require("../styles/parallax.scss"),require("../styles/project.scss"),require("../styles/home.scss");
+},{"../styles/base.scss":"hhCY","../styles/loading.scss":"KHdE","../styles/landing.scss":"VEBd","../styles/parallax.scss":"hhCY","../styles/project.scss":"QZMV","../styles/home.scss":"hhCY"}]},{},["ikcD"], null)
+//# sourceMappingURL=/script.d0b3d319.js.map
